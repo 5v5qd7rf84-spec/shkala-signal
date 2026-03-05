3997d0d7f82f95c8428db96f3196ac0461369de0
5v5qd7rf84-spec <sharata94@ukr.net>
2026-03-05 21:33:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,53 +1,39 @@
-const CACHE = 'kvp-pwa-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-180.png',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-maskable-512.png'
+const CACHE_NAME = "shkala-signal-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-180.png",
+  "./icon-maskable-512.png"
 ];
 
-self.addEventListener('install', (event) => {
+// install
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', (event) => {
+// activate
+self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE) ? caches.delete(k) : null))
-    ).then(() => self.clients.claim())
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  const req = event.request;
-  const url = new URL(req.url);
-
-  if (req.method !== 'GET' || url.origin !== location.origin) return;
-
-  const accept = req.headers.get('accept') || '';
-  const isHTML = accept.includes('text/html');
-
-  if (isHTML) {
-    event.respondWith(
-      fetch(req).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(cache => cache.put(req, copy));
-        return res;
-      }).catch(() => caches.match(req).then(m => m || caches.match('./index.html')))
-    );
-    return;
-  }
-
+// fetch
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(cache => cache.put(req, copy));
-      return res;
-    }))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
